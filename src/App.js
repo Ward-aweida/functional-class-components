dcbdba3f0e45b7ae78366829c044e7604a498e8c
ward awiedah <78215331+Ward-aweida@users.noreply.github.com>
2022-01-17 12:12:27 +0200

    functional-class-components
    finshed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PersonCard from './Components/PersonCard';
+import Counter from './Components/Counter';
+import UserForm from './UserForm';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+   
+     <br/>
+     <UserForm />
+  
+     
+    </div>
+  );
+}
+
+export default App;
